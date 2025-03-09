f366ef72f9161f6c7f8b40ff29d80987f409487a
OG <o.gojayev2000@gmail.com>
2025-03-10 00:43:54 +0100

    Add methods for retrieving, creating, and updating students in StudentController

--- Controller/StudentController.cs
+++ Controller/StudentController.cs
@@ -31,6 +31,57 @@
             }
             return student;
         }
+
+        [HttpGet("Name")]
+        public async Task<ActionResult<Student>> GetStudentByName(string Name) {
+            var student = await _context.Student.FirstOrDefaultAsync(s => s.Name == Name);
+
+            if (student == null) {
+                return NotFound();
+            }
+            return student;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            _context.Student.Add(student);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetStudent", new { Id = student.Id}, student);
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> PutStudent(int Id, Student student)
+        {
+            if (Id != student.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(Id))
+                {
+                    return NotFound();
+                } else 
+                {
+                   throw;
+                }
+            }
+            return NoContent();
+        }
+
+        // [HttpDelete("Id")]
+
+        private bool StudentExists(int Id) 
+        {
+            return _context.Student.Any(s => s.Id == Id);
+        }
     }
     
 }
